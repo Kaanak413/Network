7f4107406cc679e4c2f0f1be47f5a01b8a25116d
KaanAkbay <thedosen48@gmail.com>
2024-10-26 01:11:38 +0300

    Added multithreaded functionality to the web server

--- WebServer/webServer.py
+++ WebServer/webServer.py
@@ -1,31 +1,70 @@
 from socket import *
-import sys 
+import threading
+from threading import get_ident
 
-serverSocket = socket(AF_INET, SOCK_STREAM)
-svPort = 6789
-serverSocket.bind(('',svPort))
-serverSocket.listen(1)
-
-while True:
-    
-    print('Ready to serve...')
-    connectionSocket, addr = serverSocket.accept()
-    try:
-        message = connectionSocket.recv(1024)
-        filename = message.split()[1]
-        f = open(filename[1:])
-        outputdata = f.read()
-        connectionSocket.send(bytes('HTTP/1.1 200 OK\r\n\r\n','UTF-8'))
-
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
-        connectionSocket.send("\r\n".encode())
-        connectionSocket.close()
+class Server:
+    def __init__(self, threadCount, portNo, ipAddress=''):
+        self.threadCount = threadCount
+        self.portNo = portNo
+        self.ipAddress = ipAddress
+        self.threadPool = []
+        self.jobQueue = []
+        self.lock = threading.Lock()
+        self.condition = threading.Condition(self.lock)
         
-    except IOError:
-        connectionSocket.send(bytes("HTTP/1.1 404 Not found\r\n\r\n",'UTF-8'))
-        connectionSocket.close()
-        
+    def run(self):
+        self.createThreads()
+        mainThread = threading.Thread(target=self.MainSvThread)
+        mainThread.start()
+        mainThread.join()
 
-serverSocket.close()
-sys.exit()    
+    def MainSvThread(self):
+        serverSocket = socket(AF_INET, SOCK_STREAM)
+        serverSocket.bind((self.ipAddress, self.portNo))
+        serverSocket.listen(1)
+        print(f'Server listening on port {self.portNo}...')
+        
+        while True:
+            connectionSocket, addr = serverSocket.accept()
+            print(f'Connection received from {addr}')
+            self.giveJobToTheThread(connectionSocket)
+
+    def giveJobToTheThread(self, connectionSocket):
+        with self.condition:
+            self.jobQueue.append(connectionSocket)
+            self.condition.notify()
+
+    def work(self):
+        while True:
+            with self.condition:
+                while not self.jobQueue:
+                    self.condition.wait()
+                connectionSocket = self.jobQueue.pop(0)
+            print(f'Worker {get_ident()} is working')    
+            try:
+                message = connectionSocket.recv(1024).decode()
+                filename = message.split()[1]
+                with open(filename[1:], 'rb') as f:
+                    outputdata = f.read()
+                
+                connectionSocket.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
+                connectionSocket.sendall(outputdata)
+                connectionSocket.sendall(b"\r\n")
+                
+            except IOError:
+                connectionSocket.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+            finally:
+                connectionSocket.close()
+
+    def createThreads(self):
+        for _ in range(self.threadCount):
+            thread = threading.Thread(target=self.work)
+            thread.daemon = True
+            thread.start()
+            self.threadPool.append(thread)
+
+if __name__ == "__main__":
+    threadCount = 4
+    portNo = 6789
+    webServer = Server(threadCount, portNo)
+    webServer.run()
