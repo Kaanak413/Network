3cbadce4a33f317f1f35656d2ffe1ee74736df87
KaanAkbay <thedosen48@gmail.com>
2024-11-02 00:21:15 +0300

    Added post request

--- HttpWebProxyServer/ProxyServer.py
+++ HttpWebProxyServer/ProxyServer.py
@@ -36,64 +36,103 @@ while True:
     if not message:
         tcpCliSock.close()
         continue
-    
-    try:
-        # Parse the filename from the received request
-        filename = message.split()[1].partition("/")[2]
-        print("Requested file:", filename)
+    msgRequest = message.split('/')[0]
+    if(msgRequest == 'GET '):
+        try:
+            # Parse the filename from the received request
+            filename = message.split()[1].partition("/")[2]
+            print("Requested file:", filename)
+            
+            if filename in cache:
+                # Check if the requested URL is cached
+                cache_filename = get_cache_filename(filename)
+                # Serve the file from cache
+                with open(cache_filename, "rb") as cached_file:
+                    outputdata = cached_file.readlines()
+                    tcpCliSock.sendall(b"HTTP/1.0 200 OK\r\n")
+                    tcpCliSock.sendall(b"Content-Type: text/html\r\n\r\n")
+                    for line in outputdata:
+                        tcpCliSock.sendall(line)
+                    print('Served from cache')
+            
+            else:
+                # File is not in cache, fetch it from the web server
+                hostn = filename.replace("www.", "", 1)
+                print("Host:", hostn)
+                try:
+                    # Connect to the web server
+                    c = socket(AF_INET, SOCK_STREAM)
+                    c.connect((hostn, 80))
+                    
+                    # Send the request to the web server
+                    request = f"GET / HTTP/1.0\r\nHost: {hostn}\r\n\r\n"
+                    c.sendall(request.encode())
+                    
+                    # Receive the response from the web server
+                    response = b""
+                    while True:
+                        data = c.recv(4096)
+                        if not data:
+                            break
+                        response += data
+                    
+                    # Send the response to the client
+                    tcpCliSock.sendall(response)
+                    
+                    # Cache the response
+                    with open(cache_filename, "wb") as tmpFile:
+                        tmpFile.write(response)
+                    cache[filename] = cache_filename  # Update cache dictionary
+                    print('Fetched from web server and cached')
+                    
+                    c.close()
+                except Exception as e:
+                    print("Error: Could not fetch file from server.", e)
+                    tcpCliSock.sendall(b"HTTP/1.0 404 Not Found\r\n")
+                    tcpCliSock.sendall(b"Content-Type: text/html\r\n\r\n")
+                    tcpCliSock.sendall(b"<html><body><h1>404 Not Found</h1></body></html>")
         
-        # Check if the requested URL is cached
-        cache_filename = get_cache_filename(filename)
-        if filename in cache:
-            # Serve the file from cache
-            with open(cache_filename, "rb") as cached_file:
-                outputdata = cached_file.readlines()
-                tcpCliSock.sendall(b"HTTP/1.0 200 OK\r\n")
-                tcpCliSock.sendall(b"Content-Type: text/html\r\n\r\n")
-                for line in outputdata:
-                    tcpCliSock.sendall(line)
-                print('Served from cache')
-        
-        else:
-            # File is not in cache, fetch it from the web server
-            hostn = filename.replace("www.", "", 1)
-            print("Host:", hostn)
-            try:
-                # Connect to the web server
-                c = socket(AF_INET, SOCK_STREAM)
-                c.connect((hostn, 80))
-                
-                # Send the request to the web server
-                request = f"GET / HTTP/1.0\r\nHost: {hostn}\r\n\r\n"
-                c.sendall(request.encode())
-                
-                # Receive the response from the web server
-                response = b""
-                while True:
-                    data = c.recv(4096)
-                    if not data:
-                        break
-                    response += data
-                
-                # Send the response to the client
-                tcpCliSock.sendall(response)
-                
-                # Cache the response
-                with open(cache_filename, "wb") as tmpFile:
-                    tmpFile.write(response)
-                cache[filename] = cache_filename  # Update cache dictionary
-                print('Fetched from web server and cached')
-                
-                c.close()
-            except Exception as e:
-                print("Error: Could not fetch file from server.", e)
-                tcpCliSock.sendall(b"HTTP/1.0 404 Not Found\r\n")
-                tcpCliSock.sendall(b"Content-Type: text/html\r\n\r\n")
-                tcpCliSock.sendall(b"<html><body><h1>404 Not Found</h1></body></html>")
-    
-    except Exception as e:
-        print("Error processing request:", e)
-    
+        except Exception as e:
+            print("Error processing request:", e)
+    elif(msgRequest == 'POST '):
+                hostn = filename.replace("www.", "", 1)
+                print("Host:", hostn)
+                try:
+                    # Connect to the web server
+                    c = socket(AF_INET, SOCK_STREAM)
+                    c.connect((hostn, 80))
+                    path = "/post-endpoint"  # Replace with the target path
+                    data = "key1=value1&key2=value2"  # Data to send in the body (URL-encoded format)
+                    request = (
+                                f"POST {path} HTTP/1.1\r\n"
+                                f"Host: {hostn}\r\n"
+                                "Content-Type: application/x-www-form-urlencoded\r\n"
+                                f"Content-Length: {len(data)}\r\n"
+                                "Connection: close\r\n"
+                                "\r\n"
+                                f"{data}"
+                            )
+
+
+                    # Send the request to the web server
+                    c.sendall(request.encode())
+                    # Receive the response
+                    response = b""
+                    while True:
+                        chunk = c.recv(4096)  # Receive in chunks
+                        if not chunk:
+                            break
+                        response += chunk
+                    print("Response received from the server:")
+                    print(response.decode())
+                    
+                except Exception as e:
+                    print("Error: Could not fetch file from server.", e)
+                    tcpCliSock.sendall(b"HTTP/1.0 404 Not Found\r\n")
+                    tcpCliSock.sendall(b"Content-Type: text/html\r\n\r\n")
+                    tcpCliSock.sendall(b"<html><body><h1>404 Not Found</h1></body></html>")
+
+
     # Close the client socket
     tcpCliSock.close()
 
